5b75702394c06b07a69c89a397edbf8ac175dd0f
kevin <kivito2009@hotmail.com>
2022-04-28 17:10:37 -0500

    archivos de herouku

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(validacionDtos);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
